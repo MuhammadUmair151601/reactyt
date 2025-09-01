a9edcf59f8480ba0f1a75896724b918f7dc662fa
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-09-01 15:54:12 +0500

    usestate and counter creating practice done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,53 @@
-import React from "react";
+// import React, { useState } from "react";
 
-const App = () => {
-  const user = "umair"
-  const age = 30
-
-const changeUser = () =>{
-    console.log(user)
-    const newuser = "ahmed"
-    console.log(newuser)
-  }
-
+// ************ changing value through number**************
+// const App = () => {
   
-  return <div>
-  <h1>hello {user} your age is {age}</h1>
-  <button onClick={changeUser}>change user</button>
-  </div>
-}
+//   const [a, setA] = useState(30)
+//   const changea = () => {
+//     console.log("chal gaya:")
+//     setA(45);
+//   }
+  
+//   return <div>
+//   <h1>Hello your marks are {a}</h1>
+//   <button onClick={changea}>change user</button>
+//   </div>
+// }
+// export default App;
+// ***************************************
 
-export default App;
+// import React  from 'react'
+// import { useState } from "react";
+// const App = () => {
+  
+//   const [num, setnum]= useState(0)
+
+//   return (
+//     <div>
+//       <h1>Current number is {num}</h1>
+//       <button onClick={() => setnum + 1}>increment</button>
+//       <button onClick={() => setnum - 1}>decrement</button>
+//     </div>
+//   )
+// }
+
+// export default App
+// *********************creating counter**********************
+
+// import React, { useState } from "react";
+
+// const App = () => {
+//   const [num, setNum] = useState(0);
+
+//   return (
+//     <div>
+//       <h1>Current number is {num}</h1>
+//       <button onClick={() => setNum(num + 1)}>increment</button>
+//       <button onClick={() => setNum(num - 1)}>decrement</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
