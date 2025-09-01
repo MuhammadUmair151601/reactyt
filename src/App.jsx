dc69850fb239475a081a24fb247ba92163115222
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-09-01 12:42:15 +0500

    import export and changing values done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-function App(){
+import React from "react";
+
+const App = () => {
+  const user = "umair"
+  const age = 30
+
+const changeUser = () =>{
+    console.log(user)
+    const newuser = "ahmed"
+    console.log(newuser)
+  }
+
+  
   return <div>
-  <h1>HEllo</h1>;
-  <h2>Not hello</h2>
+  <h1>hello {user} your age is {age}</h1>
+  <button onClick={changeUser}>change user</button>
   </div>
 }
 
-export default App
+export default App;
