f74467afc33b82c488ae28f1bcdb081f37f636eb
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-09-02 11:21:33 +0500

    tailwind css added to the project

--- src/App.jsx
+++ src/App.jsx
@@ -51,3 +51,18 @@
 
 // export default App;
 
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1 className= "text-3xl text-blue-500 font-bold">Hello everyone!</h1>
+//       <h2 className= "text-3xl text-blue-500 font-bold">I am Good!</h2>
+//       <button className="px-[10px] py-[10px] border-2 border-white rounded-[5px] text-[20px] bg-gray-300 text-black">Click me</button>
+//       <button className="px-[10px] py-[10px] border-2 border-white rounded-[5px] text-[20px] bg-gray-300 text-black">Back</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
